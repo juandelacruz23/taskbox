c7d95add234c62a2eea875a4dd25fa03ab5c0f2e
Juan De la Cruz <juan.carlos.cc@outlook.com>
2021-05-28 13:37:03 -0500

    simple component

---
+++ src/components/Task.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import PropTypes from "prop-types";
+
+const Task = ({ task: { id, title, state}, onArchiveTask, onPinTask}) => {
+  return (
+    <div className={`list-item ${state}`}>
+      <label className="checkbox">
+        <input type="checkbox" name="checked" defaultChecked={state === "TASK_ARCHIVED"} disabled={true} />
+        <span className="checkbox-custom" onClick={() => onArchiveTask(id)}></span>
+      </label>
+      <div className="title">
+        <input type="text" value={title} readOnly={true} />
+      </div>
+      <div className="actions" onClick={e => e.stopPropagation()}>
+        {
+          state !== "TASK_ARCHIVED" && (
+            // eslint-disable-next-line jsx-a11y/anchor-is-valid
+            <a onClick={() => onPinTask(id)}>
+              {
+                <span className="icon-star" />
+              }
+            </a>
+          )
+        }
+      </div>
+    </div>
+  );
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    state: PropTypes.string.isRequired,
+  }),
+  onArchiveTask: PropTypes.func,
+  onPinTask: PropTypes.func,
+}
+
+export default Task;
